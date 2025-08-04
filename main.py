74ddf5cffc748aeb7f1b3bbc778b17ae897e5842
Brigitte Anne Santos <santosbrigitte@gmail.com>
2025-08-03 18:06:37 -0700

    Organize parts of main function into individual functions

--- main.py
+++ main.py
@@ -2,35 +2,35 @@ import time
 
 COMM_TXTFILE = 'weighted-scores-calculator.txt'
 
-def check_errors(file):
+def check_errors(f_lines):
     """Checks for errors in the file and returns an error statement."""
     error = None
-    if len(lines) < 2:
+    if len(f_lines) < 2:
         error = "Error: Parameters missing. See Documentation for details."
     else:
-        for i in range(1,len(lines)):
-            line_arr = lines[i].split(',')
+        for i in range(1,len(f_lines)):
+            line_arr = f_lines[i].split(',')
             if len(line_arr) != 3:
                 error = "Error: Priority category is missing data."
             elif not line_arr[1].strip().isnumeric() or not line_arr[2].strip().isnumeric():
                 error = "Error: Priority category weight or score is not a number"
     return error
 
-def build_2d_arr(received_data):
+def build_2d_arr(f_lines):
     """Builds 2d array"""
     sum_w = 0
     arr = []
     headers = []
     data_row = []
-    for i in range(len(lines)+1):
+    for i in range(len(f_lines)+1):
         if i == 0:
             headers.append('Item ID')
-            data_row.append(lines[i].strip()) # append item identifier
-        elif i == len(lines):
+            data_row.append(f_lines[i].strip()) # append item identifier
+        elif i == len(f_lines):
             headers.append('Total Weighted Score')
             data_row.append(sum_w)
         else:
-            line = lines[i].split(',')
+            line = f_lines[i].split(',')
             headers.append(line[0] + ' (' + line[1] + ')')  # add priority category and weight in paranthesis in header
             data_row.append(line[2].strip()) # append unweighted score
 
@@ -61,27 +61,37 @@ def is_sorted(arr):
         prev_line_weight_int = line_weight_int
     return True
 
-if __name__ == '__main__':
-    while True:
-        time.sleep(5)
-        with open(COMM_TXTFILE, 'r', newline='\n') as f:
-            lines = f.readlines()
-            if lines == []:
-                continue
+def read_request():
+    """Reads weighted-scores-calculator.txt for call"""
+    with open(COMM_TXTFILE, 'r', newline='\n') as f:
+        lines = f.readlines()
+        if lines == []:
+            return
 
-            error = check_errors(f)
-
+        error = check_errors(lines)
         if error:
-            with open(COMM_TXTFILE, 'w', newline='') as f:
-                f.write(error)
-            continue
+            write_error(error)
 
         if not is_sorted(lines):
             lines = sort_arr(lines)
 
         arr = build_2d_arr(lines)
 
-        with open(COMM_TXTFILE, 'w', newline='') as f:
-            f.write(str(arr))
+        write_2d_array(arr)
 
-        time.sleep(20)
+    time.sleep(10)
+
+def write_2d_array(arr_2d):
+    """Write 2d array to file"""
+    with open(COMM_TXTFILE, 'w', newline='') as f:
+        f.write(str(arr_2d))
+
+def write_error(error):
+    """Write error message to file"""
+    with open(COMM_TXTFILE, 'w', newline='') as f:
+        f.write(error)
+
+if __name__ == '__main__':
+    while True:
+        time.sleep(5)
+        read_request()
